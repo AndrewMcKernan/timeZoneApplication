c1e03d27b0c4fa728eb90fe011ca7d0382f42444
Andrew McKernan <amckerna@ualberta.ca>
2021-10-04 22:43:00 -0600

    created frontend and backend django applications
    Began work on backend, created serializers, models, and views. Views currently unimplemented. Ran initial migrations as well.

---
+++ timezoneApplication/backend/models.py
@@ -0,0 +1,9 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+# Create your models here.
+
+class Timezone(models.Model):
+    name = models.CharField(max_length=100)
+    city_name = models.CharField(max_length=100)
+    difference_to_gmt = models.IntegerField(validators=[MinValueValidator(-12), MaxValueValidator(14)])
