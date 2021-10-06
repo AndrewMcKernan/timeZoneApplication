113094938909e9ea2ef121ccedba3c7edb3f9c22
Andrew McKernan <amckerna@ualberta.ca>
2021-10-06 16:29:42 -0600

    added permissions to model

--- timezoneApplication/backend/models.py
+++ timezoneApplication/backend/models.py
@@ -9,3 +9,7 @@ class Timezone(models.Model):
     city_name = models.CharField(max_length=100)
     difference_to_gmt = models.IntegerField(validators=[MinValueValidator(-12), MaxValueValidator(14)])
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # see https://stackoverflow.com/questions/34305805/foreignkey-user-in-models
+    class Meta:
+        permissions = (
+            ("is_admin_user", "Admin User"),
+        )
