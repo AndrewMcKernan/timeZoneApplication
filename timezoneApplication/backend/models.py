b2b787013b9d3adb09fc85526320a93faa2b255d
Andrew McKernan <amckerna@ualberta.ca>
2021-10-06 16:21:36 -0600

    fixed user on timezone model

--- timezoneApplication/backend/models.py
+++ timezoneApplication/backend/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
@@ -7,4 +8,4 @@ class Timezone(models.Model):
     name = models.CharField(max_length=100)
     city_name = models.CharField(max_length=100)
     difference_to_gmt = models.IntegerField(validators=[MinValueValidator(-12), MaxValueValidator(14)])
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # see https://stackoverflow.com/questions/34305805/foreignkey-user-in-models
