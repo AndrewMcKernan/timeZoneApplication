bd687e3bf37aa7f2aedf5e46e6776dec7aa80d84
Andrew McKernan <amckerna@ualberta.ca>
2021-10-06 16:09:52 -0600

    added user to timezone model

--- timezoneApplication/backend/models.py
+++ timezoneApplication/backend/models.py
@@ -7,3 +7,4 @@ class Timezone(models.Model):
     name = models.CharField(max_length=100)
     city_name = models.CharField(max_length=100)
     difference_to_gmt = models.IntegerField(validators=[MinValueValidator(-12), MaxValueValidator(14)])
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
