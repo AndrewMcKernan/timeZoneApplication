9b09e6ecedbff383db0f8fe7b5797ed2aa545c56
Andrew McKernan <amckerna@ualberta.ca>
2021-10-06 18:18:19 -0600

    added timezone to admin site

--- timezoneApplication/backend/admin.py
+++ timezoneApplication/backend/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Timezone
+
+class TimezoneAdmin(admin.ModelAdmin):
+    fields = ['name', 'city_name', 'difference_to_gmt', 'user']
+    
+admin.site.register(Timezone, TimezoneAdmin)
